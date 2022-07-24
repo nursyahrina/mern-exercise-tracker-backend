51f8de5e8212d892363abd8550c7a799fea1ad80
nursyahrina <nursyahrina17@gmail.com>
2022-07-24 08:29:39 +0700

    move backend of mern-exercise-tracker to new repo

---
+++ server.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+const { connection } = mongoose;
+connection.once('open', () => {
+  console.log('MongoDB connection established successfully');
+});
+
+app.use(cors());
+app.use(express.json());
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
